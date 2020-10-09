11ef6ac20579dda124060cfbf5c61563fac3abd1
John Lwin <johnlwin.dev@gmail.com>
2020-10-09 00:17:29 -0700

    Add products as component level state with useState, useEffect and axios to fetch data

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
 
 const HomeScreen = () => {
+  const [products, setProduct] = useState([])
+
+  // runs as soon as homeScreen component loads
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      setProduct(data)
+    }
+    fetchProducts()
+  }, [])
+
   return (
     <>
       <h1>Latest Products</h1>
