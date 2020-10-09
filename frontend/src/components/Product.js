37664d28ffc8e27fdac4772e6b0449321f2056d3
John Lwin <johnlwin.dev@gmail.com>
2020-10-08 22:47:29 -0700

    Complete Product component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
 
-const Product = ({ product }) => {
+const Product = ({ product, color }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <a href={`/product/${product._id}`}>
@@ -12,7 +13,7 @@ const Product = ({ product }) => {
       <Card.Body>
         <a href={`/product/${product._id}`}>
           <Card.Title as='div'>
-            <strong>{product.name}</strong>
+            <strong style={{ color }}>{product.name}</strong>
           </Card.Title>
         </a>
 
@@ -27,6 +28,15 @@ const Product = ({ product }) => {
       </Card.Body>
     </Card>
   )
+}
+
+Product.defaultProps = {
+  color: '#0000ff',
+}
+
+Product.propTypes = {
+  product: PropTypes.object.isRequired,
+  color: PropTypes.string,
 }
 
 export default Product
