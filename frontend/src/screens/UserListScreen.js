c942fe355b67077afdaa64dc2d0fd62e94a08e67
John Lwin <johnlwin.dev@gmail.com>
2020-10-27 21:58:28 -0700

    Edit user list screen

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listUsers, deleteUser } from '../actions/userActions'
+import { listUsers } from '../actions/userActions'
 
 const UserListScreen = ({ history }) => {
   const dispatch = useDispatch()
@@ -12,24 +12,12 @@ const UserListScreen = ({ history }) => {
   const userList = useSelector((state) => state.userList)
   const { loading, error, users } = userList
 
-  const userLogin = useSelector((state) => state.userLogin)
-  const { userInfo } = userLogin
-
-  const userDelete = useSelector((state) => state.userDelete)
-  const { success: successDelete } = userDelete
-
   useEffect(() => {
-    if (userInfo && userInfo.isAdmin) {
-      dispatch(listUsers())
-    } else {
-      history.push('/login')
-    }
-  }, [dispatch, history, successDelete, userInfo])
+    dispatch(listUsers())
+  }, [dispatch])
 
   const deleteHandler = (id) => {
-    if (window.confirm('Are you sure')) {
-      dispatch(deleteUser(id))
-    }
+    console.log('delete')
   }
 
   return (
