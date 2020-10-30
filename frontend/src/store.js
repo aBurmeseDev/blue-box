dc75ad2b7b04965d5db986285779bf7c035344c0
John Lwin <johnlwin.dev@gmail.com>
2020-10-29 22:31:47 -0700

    Import userDeleteReducer into store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,6 +12,7 @@ import {
   userDetailsReducers,
   userUpdateProfileReducers,
   userListReducer,
+  userDeleteReducer,
 } from './reducers/userReducers'
 import {
   orderCreateReducer,
@@ -29,6 +30,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducers,
   userUpdateProfile: userUpdateProfileReducers,
   userList: userListReducer,
+  userDelete: userDeleteReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
