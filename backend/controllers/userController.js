354f0d5673f45188f5ef418220250b56fbde419e
John Lwin <johnlwin.dev@gmail.com>
2020-10-30 23:56:14 -0700

    Create getUserById user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -127,6 +127,20 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
 export {
   authUser,
   registerUser,
@@ -134,4 +148,5 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
 }
